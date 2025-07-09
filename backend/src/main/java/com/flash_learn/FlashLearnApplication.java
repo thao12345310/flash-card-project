a5508aa7856f90cb72d51b95a12eb011eb7053e1
unknown <duongphuongthao08102004@gmail.com>
2025-07-09 10:02:51 +0700

    set up project

---
+++ backend/src/main/java/com/flash_learn/FlashLearnApplication.java
@@ -0,0 +1,13 @@
+package com.flash_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashLearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/flash_learn/FlashLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flash_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
